126067c7a51209bc6365057e4669cd0287eaf62a
anorv <aur.norvilaite@gmail.com>
2023-04-21 14:37:51 +0300

    add about text

--- src/components/modules/about/About.tsx
+++ src/components/modules/about/About.tsx
@@ -1,5 +1,17 @@
+import Card from 'react-bootstrap/Card';
+
 function About() {
-  return <div className="text-center mt-2">{'About'}</div>;
+  const text =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt sapiente quisquam quasi voluptas delectus voluptate. Tempore perferendis eum aperiam quia eius sapiente commodi, quibusdam laudantium expedita suscipit ea quam eveniet?';
+  return (
+    <Card className="m-2">
+      <Card.Body>
+        <div>
+          <p className="text-center">{text}</p>
+        </div>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default About;
